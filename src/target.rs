6aab4867b0d0deced7525feee5fa26e4a6cdce6d
Brian Campbell <Brian.Campbell@ed.ac.uk>
2020-09-21 23:00:39 +0100

    Add little-endian bit to SCTLR_EL3

--- src/target.rs
+++ src/target.rs
@@ -158,8 +158,8 @@ impl Target for Morello {
             if reg == "SCTLR_EL3" {
                 let reserved_mask = 0b1111_1111_1111_1111_1110_0100_1100_1111_0011_0101_1001_0111_1100_0111_1011_0000;
                 let reserved_vals = 0b0000_0000_0000_0000_0000_0000_0000_0000_0011_0000_1000_0101_0000_0000_0011_0000;
-                // EL3 instructions non-cachable, EL3 non-cachable, EL3 stage 1 translation disabled
-                let fixed_mask =    0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0001_0000_0000_0101;
+                // Little endian at EL3, EL3 instructions non-cachable, EL3 non-cachable, EL3 stage 1 translation disabled
+                let fixed_mask =    0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0010_0000_0000_0001_0000_0000_0101;
                 let fixed_vals =    0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
                 solver.add(Def::Assert(Exp::Eq(
                     Box::new(Exp::Bvand(
